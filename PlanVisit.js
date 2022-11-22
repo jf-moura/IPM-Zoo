6003c5d410f264ec5c1a3afecddd7b2beb3a62b1
joaoncfsantos <santos.joaonuno2000@gmail.com>
2022-11-22 12:17:42 +0000

    orgranize visit page with map and list well placed

--- PlanVisit.js
+++ PlanVisit.js
@@ -1,14 +1,44 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+//The orderToVisit[0] animal will be the animal to be visited at first place, the orderToVisit[1] is the second and so on
+const orderToVisit = [
+  "bear",
+  "elephant",
+  "cheetah",
+  "giraffe",
+  "hippo",
+  "lion",
+  "rhino",
+  "tiger",
+  "dolphin",
+  "zebra",
+  "primate",
+  "deer",
+  "panda",
+  "camel",
+];
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+const NO_ANIMALS_SELECTED =
+  "You have to selected at least one animal in order to oraganize your visit!";
+
+var selectedAnimals = [];
+
+//Function that handles the selection or deselection of the animals
+function selectAnimal(animal) {
+  const index = selectedAnimals.indexOf(animal);
+  index == -1 ? selectedAnimals.push(animal) : selectedAnimals.splice(index, 1);
+}
+
+var animalsOrdered = [];
+
+function handleSubmit() {
+  if (selectedAnimals.length == 0) {
+    return alert(NO_ANIMALS_SELECTED);
+  }
+
+  for (let i = 0; i < orderToVisit.length; i++) {
+    if (selectedAnimals.includes(orderToVisit[i]))
+      animalsOrdered.push(orderToVisit[i]);
+  }
+  console.log("Animais escolhidos");
+  console.log(animalsOrdered);
+  location.href = "rota.html";
 }
